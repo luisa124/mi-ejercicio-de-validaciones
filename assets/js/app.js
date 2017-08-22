9ce083ca882d72c142be7c1b0cef3633ddc0d232
Luisa124 <luisa.mayta.1212@gmail.com>
2017-08-22 12:56:31 -0700

    validaciones

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var input = document.getElementById("nombreInput").value; 
+
+function myboton(){
+    alert("pon tu nombre");
+}
+
+function myBoton(nombreInput){
+    var nombreInput = "";
+    for (var i = 0; i <= nombreInput.length; i++){
+      document.getElementById("nombreInput");
+    }
+        
+}
